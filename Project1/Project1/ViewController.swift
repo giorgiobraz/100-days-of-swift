9e1ed86e919e0a2342f7d3a7e6296c6c466e6b6e
Giorgio Braz <29463682+giorgiobraz@users.noreply.github.com>
2022-01-26 21:17:23 -0300

    Day 18 - Project 1, part three

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UITableViewController {
                 pictures.append(item)
             }
         }
+        pictures.sort()
         print(pictures)
     }
     
@@ -43,6 +44,7 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
+            vc.title = "Picture \(indexPath.row + 1) of \(pictures.count)"
             navigationController?.pushViewController(vc, animated: true)
         }
     }
