c1d48e6906595f42c0224152fcdf18e762788c48
Giorgio Braz <29463682+giorgiobraz@users.noreply.github.com>
2022-01-28 21:45:09 -0300

    Day 20 - Project 2, part two

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,14 +30,37 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.lightGray.cgColor
 //        button3.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0)
         
-        askQuestion()
+        askQuestion(action: nil)
     }
     
-    func askQuestion(){
+    func askQuestion(action: UIAlertAction!){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
+    
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
 }
 
